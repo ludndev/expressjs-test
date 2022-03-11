8de5a7ae8902859d0ec1bb0dc791bd4a9b85c7d2
ludndev <ludndev@gmail.com>
2022-03-11 17:06:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static('public'));
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
